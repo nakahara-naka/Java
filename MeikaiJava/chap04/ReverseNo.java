a3ec0739c64345d0c2bd60ab36565c4bdf7ce083
nakahara-naka <y.dust.and.bones@gmail.com>
2020-11-14 21:45:04 +0900

    4-3 for文

---
+++ MeikaiJava/chap04/ReverseNo.java
@@ -0,0 +1,21 @@
+//正の整数値を読み込んで逆順に表示
+import java.util.Scanner;
+class ReverseNo{
+  public static void main(String[] args){
+    Scanner stdIn = new Scanner(System.in);
+
+    System.out.println("正の整数値を逆順に表示します。");
+    int x;
+    do{
+      System.out.print("正の整数値");
+      x = stdIn.nextInt();
+    }while(x <= 0);
+
+    System.out.print("逆から読むと");
+    while(x > 0){
+      System.out.print(x % 10); //xの最下位行を表示
+      x /= 10; //xを１０で割る
+    }
+    System.out.println("です。");
+  }
+}
