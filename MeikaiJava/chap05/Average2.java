54914fa07f77d5eda43c8f6cc8995416e511df50
nakahara-naka <y.dust.and.bones@gmail.com>
2020-11-17 21:16:48 +0900

    基本型と演算

---
+++ MeikaiJava/chap05/Average2.java
@@ -0,0 +1,14 @@
+//二つの整数値の平均値を実数で求める（キャスト演算子を利用する）
+import java.util.Scanner;
+class Average2{
+  public static void main(String[] args){
+    Scanner stdIn = new Scanner(System.in);
+
+    System.out.println("整数値xとyの平均値を求めます。");
+    System.out.print("xの値："); int x = stdIn.nextInt();
+    System.out.print("yの値："); int y = stdIn.nextInt();
+
+    double ave = (double)(x + y) / 2;
+    System.out.println("整数値xとyの平均値は" + ave + "です。");
+  }
+}
---
+++ MeikaiJava/chap05/CarriageReturn.java
@@ -0,0 +1,7 @@
+class CarriageReturn{
+  public static void main(String[] args){
+    System.out.print("ABCDEFGHIJKLMN");
+    System.out.println("\r12345");
+
+  }
+}
