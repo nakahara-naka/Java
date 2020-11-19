ae347fc98b462f9133ba3b8bf004eab2abf62977
nakahara-naka <y.dust.and.bones@gmail.com>
2020-11-19 15:02:00 +0900

    他メソッドの呼び出し List7-9 途中

---
+++ MeikaiJava/chap07/FinalParameter.java
@@ -0,0 +1,12 @@
+//final仮引数に値を代入できないことを確認
+class FinalParameter{
+  //--- ３つの仮引数の値の和を求める ---//
+  static int sumOf(final int x, final int y, final int z){
+   // x = 10; //これをコメント外すとコンパイルエラーになる
+    return x + y + z;
+  }
+
+  public static void main(String[] args){
+    System.out.println(sumOf(1, 2, 3));
+  }
+}
