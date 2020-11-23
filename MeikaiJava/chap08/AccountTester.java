ae1d7f05735fec38920249f075fa7708be625d97
nakahara-naka <y.dust.and.bones@gmail.com>
2020-11-23 12:55:06 +0900

    銀行口座クラス（第二版）

--- MeikaiJava/chap08/AccountTester.java
+++ MeikaiJava/chap08/AccountTester.java
@@ -1,12 +1,20 @@
 //銀行口座クラス（第一版）とそれをテストするクラス
 
 
+//銀行口座クラス（第一版）
+class Account{
+  String name; //口座名義
+  String no;   //口座番号
+  long balance;//預金残高
+}
+
 //銀行口座クラスをテストするクラス
 class AccountTester{
   public static void main(String[] args){
     Account adachi = new Account();   //足立君の口座
     Account nakata = new Account();   //仲田君の口座
 
+    /*
     adachi.name    = "足立幸一";
     adachi.no      = "123456";
     adachi.balance = 1000;
@@ -17,6 +25,7 @@ class AccountTester{
 
     adachi.balance -= 200;
     nakata.balance += 100;
+    */
 
     System.out.println("■足立君の口座");
     System.out.println("  口座名義：" + adachi.name);
@@ -29,11 +38,4 @@ class AccountTester{
     System.out.println("  預金残高：" + nakata.balance);
 
   }
-}
-
-//銀行口座クラス（第一版）
-class Account{
-  String name; //口座名義
-  String no;   //口座番号
-  long balance;//預金残高
 }
